efa9f77cb060f736ba4497b9b7e6a22c3a2470df
Khazhainov <danil.khazhainov@gmail.com>
2020-07-30 21:15:17 +0300

    init DB

--- src/main/resources/initDB.sql
+++ src/main/resources/initDB.sql
@@ -3,10 +3,36 @@ CREATE DATABASE IF NOT EXISTS crud;
 
 USE crud;
 
-CREATE TABLE IF NOT EXISTS users
-(
-    id        INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS users (
+    id        INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name      VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    email     VARCHAR(255) NOT NULL
-);
+    email     VARCHAR(255) NOT NULL,
+    password  VARCHAR(255) NOT NULL
+)
+    ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS roles (
+    id   INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role VARCHAR(100) NOT NULL
+)
+    ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS user_roles (
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+
+    UNIQUE (user_id, role_id)
+)
+    ENGINE = InnoDB;
+
+INSERT INTO users VALUES (1, 'Danil', 'Khazhainov', 'danil.khazhainov@gmail.com', '$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG');
+
+INSERT INTO roles VALUES (1, 'ROLE_USER');
+INSERT INTO roles VALUES (2, 'ROLE_ADMIN');
+
+INSERT INTO user_roles VALUES (1, 2);
